faea08081d8efd79961bf9ab0fb5c523885e9f92
AndreaStudy <wjdgnstjr45@gmail.com>
2024-11-25 06:43:31 +0000

    feat: 멘토링 상세 페이지 멘토 프로필 추가

---
+++ apps/web/src/actions/profile/getProfileData.ts
@@ -0,0 +1,56 @@
+'use server';
+import { commonResType } from '@components/types/ResponseTypes';
+import { getServerSession } from 'next-auth';
+import { options } from 'src/app/api/auth/[...nextauth]/options';
+interface userProfileType {
+  nickName: string;
+  profileImageUrl: string;
+}
+
+export const getProfileIamge = async (
+  uuid: string
+): Promise<userProfileType> => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/member-query-service/api/v1/memberInfo/profileImage`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'userUuid': uuid,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Failed to post profile image');
+  }
+
+  const data = (await response.json()) as commonResType<userProfileType>;
+  return data.result;
+};
+
+export const getMyProfileIamge = async (
+  profileImage: string
+): Promise<userProfileType> => {
+  const session = await getServerSession(options);
+  const menteeUuid = session?.user.uuid;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/member-query-service/api/v1/memberInfo/profileImage`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'userUuid': menteeUuid,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Failed to post profile image');
+  }
+
+  const data = (await response.json()) as commonResType<userProfileType>;
+  return data.result;
+};
--- apps/web/src/components/pages/main/mentoring/MentoProfile.tsx
+++ apps/web/src/components/pages/main/mentoring/MentoProfile.tsx
@@ -1,19 +1,24 @@
+import { getProfileIamge } from 'src/actions/profile/getProfileData';
 import Share from '../../../assets/icons/Share';
 import FitImage from '../../../ui/image/fit-image';
-export default function MentoProfile({ mentorUuid }: { mentorUuid: string }) {
+export default async function MentoProfile({
+  mentorUuid,
+}: {
+  mentorUuid: string;
+}) {
   //멘토 프로필 요청하기
-  const mentorprofile = 1;
+  const mentorProfile = await getProfileIamge(mentorUuid);
+  console.log('mentorProfile', mentorProfile);
   return (
     <div className="space-y-4">
-      <div className="hidden sm:block relative w-full aspect-square rounded-lg overflow-hidden">
-        <FitImage
-          src="/assets/images/intro1.svg"
-          alt="Profile"
-          className="object-cover"
-        />
-      </div>
+      <FitImage
+        src={mentorProfile.profileImageUrl}
+        alt="Profile"
+        className="object-cover p-4"
+      />
+
       <div>
-        <h1 className="text-xl font-bold">@Jason Ahn</h1>
+        <h1 className="text-xl font-bold">@ {mentorProfile.nickName}</h1>
         <div className="flex items-center gap-2 text-sm text-gray-600">
           <span>78K Reviews</span>
           <span>•</span>
